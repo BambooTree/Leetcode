5993e66531d429816907ccdde3afc2105fff5d4b
BambooTree/koneti <60153392+BambooTree@users.noreply.github.com>
2020-02-05 06:50:22 +0530

    Add files via upload

---
+++ First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> map;
+        for (int i = 0; i < s.length(); i++)
+         {
+            map[s[i]]++;
+        }
+        for (int i = 0; i < s.length(); i++)
+         {
+            if (map[s[i]] == 1) 
+                return i;
+        }
+        return -1;
+    }
+};
+
+int main() {
+/* 
+s = "loveleetcode",
+return 2.
+*/
+   
+        string s = "loveleetcode";
+        
+        int ret = Solution().firstUniqChar(s);
+
+       cout << "ret "<<ret<<endl;
+    
+    return 0;
+}
